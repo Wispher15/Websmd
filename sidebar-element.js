b85f79ed152ff6289c534c2e6a12972874a6062f
Wispher15 <163160536+Wispher15@users.noreply.github.com>
2026-01-17 17:13:47 +0800

    Create sidebar-element.js

---
+++ sidebar-element.js
@@ -0,0 +1,69 @@
+class SideBar extends HTMLElement {
+    connectedCallback() {
+        this.innerHTML = `
+        <div class="sidebar">
+            <div class="sidebar-header">ONLINE SYSTEM SAMARINDA</div>
+            <ul class="menu-list">
+                <li><a href="index.html" class="menu-item" id="nav-index">Pengumuman!!</a></li>
+
+                <li class="menu-item-wrapper">
+                    <a href="#" class="menu-item">Evaluasi Sales <span>›</span></a>
+                    <ul class="submenu">
+                        <li><a href="sales-harian.html" class="submenu-item">Sales Harian</a></li>
+                        <li><a href="perbandingan-bin.html" class="submenu-item">Perbandingan 2 Bin</a></li>
+                        <li><a href="trend-sales.html" class="submenu-item">Trend Sales Unit</a></li>
+                        <li><a href="p3.html" class="submenu-item">P3</a></li>
+                    </ul>
+                </li>
+
+                <li class="menu-item-wrapper">
+                    <a href="#" class="menu-item">Administrasi <span>›</span></a>
+                    <ul class="submenu">
+                        <li><a href="pk.html" class="submenu-item">PK</a></li>
+                        <li><a href="ccs.html" class="submenu-item">CCS</a></li>
+                        <li><a href="kmo.html" class="submenu-item">KMO</a></li>
+                        <li><a href="usi-oral.html" class="submenu-item">USI</a></li>
+                    </ul>
+                </li>
+
+                <li class="menu-item-wrapper">
+                    <a href="#" class="menu-item">Akun + Kunjungan <span>›</span></a>
+                    <ul class="submenu">
+                        <li><a href="kunjungan-akun.html" class="submenu-item">Kunjungan (Akun)</a></li>
+                        <li><a href="tambah-alamat.html" class="submenu-item">Tambah Alamat ID</a></li>
+                        <li><a href="buat-alamat.html" class="submenu-item">Buat Alamat ID</a></li>
+                        <li><a href="tambah-outlet.html" class="submenu-item">Tambah Outlet ID</a></li>
+                        <li><a href="master-outlet.html" class="submenu-item">Master Outlet</a></li>
+                    </ul>
+                </li>
+
+                <li><a href="stok-distributor.html" class="menu-item" id="nav-stok">Stok Distributor</a></li>
+                <li><a href="hitung-sales.html" class="menu-item" id="nav-hitung">Hitung Sales</a></li>
+                <li><a href="cuti.html" class="menu-item" style="color: #ef4444; font-weight: 700;">Cuti-Cuti !!!</a></li>
+                <li><a href="bku.html" class="menu-item">BKU</a></li>
+                <li><a href="ppf.html" class="menu-item">PPF</a></li>
+            </ul>
+        </div>
+        `;
+
+        this.setActiveMenu();
+    }
+
+    setActiveMenu() {
+        // Otomatis deteksi halaman aktif
+        const currentPage = window.location.pathname.split("/").pop() || "index.html";
+        const links = this.querySelectorAll('a');
+        links.forEach(link => {
+            if (link.getAttribute('href') === currentPage) {
+                link.classList.add('active');
+                // Buka submenu jika item yang aktif ada di dalamnya
+                const parentSub = link.closest('.submenu');
+                if (parentSub) {
+                    parentSub.style.display = 'block';
+                }
+            }
+        });
+    }
+}
+
+customElements.define('main-sidebar', SideBar);
